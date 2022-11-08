8c202fca1dd72dfd8c09c3eabfd951d8cb67e481
Tibor22 <tothtibitravel@gmail.com>
2022-11-08 14:14:26 +0000

    first-commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
+import { createClient } from 'contentful';
+
+export async function getStaticProps() {}
+
 export default function Recipes() {
-  return (
-    <div className="recipe-list">
-      Recipe List
-    </div>
-  )
+	return <div className='recipe-list'>Recipe List</div>;
 }
